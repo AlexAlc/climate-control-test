ea85fb0c3ae7795b5717b891848e6aaca7423bd2
AlexAlc <uramesialex@gmail.com>
2023-10-02 22:07:45 +0200

    Add cooling actuator

--- actuators.py
+++ actuators.py
@@ -23,3 +23,40 @@ class evaporatorActuator:
         # Empty function to simulate a real call to hardware actuator or corresponding subsystem
         pass
         
+
+class coolingActuator:
+    """
+    Simple simulation of a cooling system which can create cool air.
+    TODO: Future implementation using abstract class with generic structure common to cooling and heating and create derived class using inheritance.
+    """
+
+    def __init__(self):
+        self.compressor_states = ['OFF','MIN','MED','MAX']  # Different states of operation possible for compressor
+        self.compressor = self.compressor_states[0]         # Current assigned state of operation
+        self.condenser_fans = 0                             # Fans of cooling stage. Possible values between 0 (min) and 100 (max)
+
+    def setCooling(self, value):
+        # Saturate values between working range
+        if value < 0:
+            self.condenser_fans = 0
+        elif value > 100:
+            self.condenser_fans = 100
+        else:
+            self.condenser_fans = value
+
+        # Calculate corresponding working mode as discretization of whole range.
+        fuzzy_value = 1 + self.condenser_fans / (100 / 3);
+
+        # Saturate value
+        if fuzzy_value >= 4:
+            fuzzy_value = 3
+
+        self.compressor = self.compressor_states[int(fuzzy_value)]
+
+        # After setting up, send actual command to set hardware working state
+        self.placeholderCoolingHW()
+
+    def placeholderCoolingHW(self):
+        # Empty function to simulate a real call to hardware actuator or corresponding subsystem
+        pass
+        
