73f5f2dde94f950430f18bf7e3d28f5fe63b261f
AlexAlc <uramesialex@gmail.com>
2023-10-02 22:08:07 +0200

    Add heating actuator

--- actuators.py
+++ actuators.py
@@ -60,3 +60,40 @@ class coolingActuator:
         # Empty function to simulate a real call to hardware actuator or corresponding subsystem
         pass
         
+
+class heatingActuator:
+    """
+    Simple simulation of a heating system which can create hot air.
+    TODO: Future implementation using abstract class with generic structure common to cooling and heating and create derived class using inheritance.
+    """
+
+    def __init__(self):
+        self.resistor_states = ['OFF','MIN','MED','MAX']    # Different states of operation possible for resistor
+        self.resistor = self.resistor_states[0]             # Current assigned state of operation
+        self.water_pump = 0                                 # Pump of heating stage. Possible values between 0 (min) and 100 (max)
+
+    def setHeating(self, value):
+        # Saturate values between working range
+        if value < 0:
+            self.water_pump = 0
+        elif value > 100:
+            self.water_pump = 100
+        else:
+            self.water_pump = value
+
+        # Calculate corresponding working mode as discretization of whole range.
+        fuzzy_value = 1 + self.water_pump / (100 / 3);
+
+        # Saturate value
+        if fuzzy_value >= 4:
+            fuzzy_value = 3
+            
+        self.resistor = self.resistor_states[int(fuzzy_value)]
+
+        # After setting up, send actual command to set hardware working state
+        self.placeholderHeatingHW()
+
+    def placeholderHeatingHW(self):
+        # Empty function to simulate a real call to hardware actuator or corresponding subsystem
+        pass
+        
